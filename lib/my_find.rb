d976dd8d80780f6a5f9f691dd18a041edbc6f6bb
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 03:53:26 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,15 @@ require 'pry'
 
 def my_find(collection)
 
+  index = 0
+  
+  while index < collection.length
+    if yield(collection[index]) == true
+      return collection [index]
+    end
+    
+    index += 1
+  end
 end
+
+collection = 
